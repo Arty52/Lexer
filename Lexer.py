4455ca5440a2372b677953e3d13cb7f650afbfcb
Arty52 <artgrichine@gmail.com>
2015-02-27 12:17:07 -0800

    check_seperator added

--- Lexer.py
+++ Lexer.py
@@ -17,7 +17,51 @@ def identifyType(c):
             'read' : 'keyword',
             'while' : 'keyword',
            }
-          
+           
+#dfs for real, int, ident
+#def fsm_operator():
+#    state_table = []
+    
+
+#input: list of elements to process
+#output: two lists, tokens and lexemes
+def lexer(todo):
+    #from p46 of slides
+    tokens = []
+    lexemes = []
+    
+    for current in range(len(todo)):
+        token = todo[current]
+        if check_seperator(token):
+            tokens.append(token)
+    print(tokens)
+    
+    #begin
+    #   repeat
+    #       getchar()
+    #       if input char terminates a token AND it is an accepting state then
+    #           isolate the token/lexeme
+    #           decrement the CP if necessary
+    #       else lookup FSM (current state, input char)
+    #   until (token found) or (no more input)
+    
+    #   if token found then 
+    #       return token
+    #end
+
+#input: character from todo
+#output:
+def check_seperator(c):
+    if c == '(' or c == ')' or c == '{' or c == '}' or c == '[' or c == ']' or c == ':' or c == ';' or c == ',':
+        return True
+    else:
+        return False
+
+#def check_operator():
+
+#def check_keywork():
+
+
 #process file and prepare list of characters to process
 #input: none
 #output: List of characters that are in text file          
@@ -26,13 +70,13 @@ def process_file():
     todo = []
     
     #open file
-    #    with open(sys.argv[1]) as fh:     #implicitly open and close the file
-    with open(input('Enter file you would like to open: ')) as fh:
-#    with open('sample.txt') as fh:          #implicitly open and close the file
+#    with open(sys.argv[1]) as fh:     #implicitly open and close the file from commandline
+#    with open(input('Enter file you would like to open: ')) as fh:
+    with open('sample.txt') as fh:          #implicitly open and close the file
         if (fh): 
             print('Open!')
             for i in fh:
-                line = i.rstrip()       #strip the EOL
+                line = i.strip()       #strip the 
                 file.append(line)
 #                print(line)            #DEBUG
         else: 
@@ -47,10 +91,10 @@ def process_file():
     return todo
 
 def main():
-    todo = []   #list of characters left to process
+    todo = []             #list of characters left to process
     todo = process_file()
     print(todo)
-
+    lexer(todo)
 
 if __name__ == '__main__':
     main()
